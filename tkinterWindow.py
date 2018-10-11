6f46f421b9ae398752b1cff189fad4683fbb5a4b
T-Shinkulu5566 <manpuku5566@gmil.com>
2018-10-11 16:24:22 +0900

    Windows画面進行３

--- tkinterWindow.py
+++ tkinterWindow.py
@@ -1,26 +1,57 @@
-# ここでは画面だけ作成したい。
-# この画面から、各処理を読み出す形にしたい。
-import sys
-import tkinter
-from tkinter import font
+import os,sys
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
 
-root = tkinter.Tk()
-root.title("抽出")
-root.geometry("500x400+400+200")
+# 参照ボタンのイベント
+# button1クリック時の処理
+def button1_clicked():
+    fTyp = [("","*")]
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    filepath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+    file1.set(filepath)
 
-#ラベル
-Static1 = tkinter.Label(text=u'test')
-#Static1 = tkinter.Label(font=20)
-Static1.place(x=50, y=50)
+# button2クリック時の処理
+def button2_clicked():
+    messagebox.showinfo('FileReference Tool', u'参照ファイルは↓↓\n' + file1.get())
 
-Static2 = tkinter.Label(text=u'test2')
-Static2.place(x=50, y=100)
+if __name__ == '__main__':
+    # rootの作成
+    root = Tk()
+    root.title('抽出')
+    root.geometry("550x400+400+200")
 
-#ボタン
-Button = tkinter.Button(text=u'参照1')
-Button.place(x=300, y=50)
+    # Frame1の作成
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
 
-Button = tkinter.Button(text=u'参照2')
-Button.place(x=300, y=100)
+    # 参照ボタンの作成
+    button1 = ttk.Button(root, text=u'参照', command=button1_clicked)
+    button1.grid(row=0, column=3)
 
-root.mainloop()
+    # ラベルの作成
+    # 「ファイル」ラベルの作成
+    s = StringVar()
+    s.set('ファイル>>')
+    label1 = ttk.Label(frame1, textvariable=s)
+    label1.grid(row=0, column=0)
+
+    # 参照ファイルパス表示ラベルの作成
+    file1 = StringVar()
+    file1_entry = ttk.Entry(frame1, textvariable=file1, width=50)
+    file1_entry.grid(row=0, column=2)
+
+    # Frame2の作成
+    frame2 = ttk.Frame(root, padding=(0,5))
+    frame2.grid(row=1)
+
+    # Startボタンの作成
+    button2 = ttk.Button(frame2, text='Start', command=button2_clicked)
+    button2.pack(side=LEFT)
+
+    # Cancelボタンの作成
+    button3 = ttk.Button(frame2, text='Cancel', command=quit)
+    button3.pack(side=LEFT)
+
+    root.mainloop()
